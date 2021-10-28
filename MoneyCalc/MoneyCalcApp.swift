15f81ed052a069badb40a65014556203a9b43c53
Barry Hall <microsnout@gmail.com>
2021-10-28 13:54:44 -0400

    Initial Commit

---
+++ MoneyCalc/MoneyCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyCalcApp.swift
+//  MoneyCalc
+//
+//  Created by Barry Hall on 2021-10-28.
+//
+
+import SwiftUI
+
+@main
+struct MoneyCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
